11ae3242b7205784d553b3d4a55041b935a1bc55
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-07-03 15:36:59 +0200

    Creazione del Model e Controller

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Train;
+
+class PageController extends Controller
+{
+    public function home()
+    {
+
+        $treni = Train::all();
+
+        $dati = [
+            'trein' => $treni
+        ];
+
+        return view('home', $dati);
+    }
+}
