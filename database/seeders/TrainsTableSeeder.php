026ff3604953dc2f341580b855621742003a80c7
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-07-03 16:04:46 +0200

    seeder

---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Train;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Testing\Fakes\Fake;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $newTrain = new Train();
+        $newTrain->Azienda = 'italo';
+        $newTrain->Stazione_partenza = 'trani';
+        $newTrain->Stazione_arrivo = 'foggia';
+        $newTrain->Orario_partenza = '12:05';
+        $newTrain->Orario_arrivo = '14:00';
+        $newTrain->Codice_treno = 'fwheeu67';
+        $newTrain->Numero_carNumero_carrozzerozze = 5;
+        $newTrain->In_orario = 1;
+        $newTrain->Cancellato = 0;
+    }
+}
